04c46aacb4bc9cd00b5af7b55905596e6a81bedc
nacho-1899 <juanignacioalonso1899@gmail.com>
2022-10-24 13:17:33 -0300

    abance

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,51 @@
+function finalizarCompra(){
+   // let cantidad = document.getElementById("count").value;
+    //let enviop = document.getElementById("premium").value;
+   // let envioe = document.getElementById("express").value;
+    //let envioes = document.getElementById("estandar").value;
+    let numero = document.getElementById("calle").value;
+    let calle = document.getElementById("numero").value;
+    let esquina = document.getElementById("esquina").value;
+    if(  numero =="" || calle =="" || esquina =="" ){
+        //document.getElementById("count").style.borderColor = "red";
+        //document.getElementById("premium").style.borderColor = "red";
+       // document.getElementById("express").style.borderColor = "red";
+        //document.getElementById("estandar").style.borderColor = "red";
+        document.getElementById("calle").style.borderColor = "red";
+        document.getElementById("numero").style.borderColor = "red";
+        document.getElementById("esquina").style.borderColor = "red";
+    }else{
+    }
+    
+    
 
+
+
+
+
+
+}
+function desactivarTransferencia(){
+    let input = document.getElementById("ncuenta");
+    input.disabled = true;
+    let tarjeta = document.getElementById("ntarjeta");
+    let vencimiento = document.getElementById("vencimiento");
+    let codigo = document.getElementById("codigo");
+    tarjeta.disabled = false;
+    vencimiento.disabled = false;
+    codigo.disabled = false;
+
+}
+function desactivarTarjeta(){
+    let tarjeta = document.getElementById("ntarjeta");
+    let vencimiento = document.getElementById("vencimiento");
+    let codigo = document.getElementById("codigo");
+    tarjeta.disabled = true;
+    vencimiento.disabled = true;
+    codigo.disabled = true;
+    let input = document.getElementById("ncuenta");
+    input.disabled = false;
+}
 function subTotal(){ // muestra el subtotal en el carrito y tabla de costos
     let subtotal = 0;
     subtotal = parseInt((document.getElementById("count").value)*(productArray[0].unitCost));
@@ -32,8 +79,6 @@ function showCart(){ // muestra las propiedades del producto
     counthtml.value=productArray[0].count;
     showImagen();
     subTotal();
-    
-    
 }
 
 
@@ -71,24 +116,53 @@ document.addEventListener("DOMContentLoaded", function(){
 
         })
         document.getElementById("premium").addEventListener("change", function () {
-             porcentajeEnvio = 0.15;
+            porcentajeEnvio = 0.15;
             subTotal();
         });
         document.getElementById("express").addEventListener("change", function () {
-             porcentajeEnvio = 0.07;
+            porcentajeEnvio = 0.07;
             subTotal();
         });
         document.getElementById("estandar").addEventListener("change", function () {
-             porcentajeEnvio = 0.05;
+            porcentajeEnvio = 0.05;
             subTotal();
         });
-    
+        document.getElementById("tarjeta").addEventListener("change", function () {
+            
+            desactivarTransferencia();
+        });
+        document.getElementById("selecTransferencia").addEventListener("change", function () {
+            desactivarTarjeta();
+            
+        });
+        document.getElementById("finalizada").addEventListener("click", function () {
+            finalizarCompra();
+            
+        });
 
-
-
-
-
+        document.getElementById("guardar").addEventListener("click", function () {
+            let desaparece = document.getElementById("seleccion");
+            desaparece.style.display= none;
+            
+            //let modal = document.getElementById("EjemploModal");
+            //modal.style.display="inherit"
+            //let tarjeta = document.getElementById("tarjeta")
+            //let trasferencia = document.getElementById("pago")   
+           // if (tarjeta == true){
+           //     opcion = document.getElementById("selecionado")
+            //    opcion.innerHTML == "Tarjeta de credito"
+            //    let desaparece = document.getElementById("seleccion");
+            //    desaparece..style.display= none;
+           // }else{
+            //    trasferencia == true;
+            //    opcion = document.getElementById("selecionado")
+           //     opcion.innerHTML == "Transferencia bancaria"
+            //    let desaparece = document.getElementById("seleccion");
+            //    desaparece.style.display = none;        
+            
+        });
 });
+// falta que guarde la opcion tarjeta o trasferencia y la agregre al htmml
 
 
 
