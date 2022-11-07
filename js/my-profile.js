7579d200c29af1959becd5b48714a20c9e554fdc
nacho-1899 <juanignacioalonso1899@gmail.com>
2022-11-07 17:35:14 -0300

    entrega 7 pronta

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,47 +1,53 @@
+function mostrar(){
+  let datosUsuario = JSON.parse(localStorage.getItem("datitos"));
+  document.getElementById('pnombre').value=datosUsuario.nombre;
+  document.getElementById('papellido').value=datosUsuario.apellido;
+}
+
 document.addEventListener("DOMContentLoaded", function(){
 
     let usuario = localStorage.getItem("name");
-    let datosUsuario = localStorage.getItem("datitos");
     let email = localStorage.getItem("email");
     let inputEmail = document.getElementById("email");
     inputEmail.value=email;
     
 
-        if (usuario == null){
-        alert("No hay nadie logeado");
-        location.href="login.html";
-        }else{
-        document.getElementById('usuario').innerHTML = usuario;
-        }
-
-        document.getElementById('cerrar').addEventListener('click',()=>{
-            alert("Cerrando Sesion");
-            localStorage.clear();
-            window.location.href="login.html";
-        })
     
+    if (usuario == null){
+    alert("No hay nadie logeado");
+    location.href="login.html";
+    }else{
+    document.getElementById('usuario').innerHTML = usuario;
+    mostrar();
+    }
+
+    document.getElementById("cerrar").addEventListener('click',()=>{
+        alert("Cerrando Sesion");
+        localStorage.clear();
+        window.location.href="login.html";
+    });
 });
+
+
+
 function valido(){
     var datos = {
         nombre:"",
         apellido:"",
-        email:"",
 
     };
     datos.nombre=document.getElementById('pnombre').value;
     datos.apellido=document.getElementById('papellido').value;
-    datos.email=document.getElementById('email').value;
     localStorage.setItem('datitos', JSON.stringify(datos));
-
 }
 
 (function () {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    
     var forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
+    
     Array.prototype.slice.call(forms)
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
